04dffe5da2101ebb726afa5ef3f7036365915965
Wouter Nuijten <wouternuijten@gmail.com>
2023-08-14 10:35:44 +0200

    Update BitSetTuples.jl

--- src/BitSetTuples.jl
+++ src/BitSetTuples.jl
@@ -56,7 +56,7 @@ end
 
 
 """
-    convert_to_constraint(constraint::BitSetTuple, max_element::Int)
+    get_membership_sets(constraint::BitSetTuple, max_element::Int)
 
 Checks all elements from 1 to max_element and returns a BitSetTuple where each element is a BitSet containing all the groups that contain that element.
 """
