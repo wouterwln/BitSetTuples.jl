faaafa4b15a8ecf129730d5b4c7c4f04093541ac
Wouter Nuijten <wouternuijten@gmail.com>
2024-02-26 17:12:14 +0100

    Split in-place benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,46 +7,74 @@ Random.seed!(0)
 const SUITE = BenchmarkGroup()
 
 
-for i in (2, 5, 10, 100)
+for i in (2, 5, 10, 100, 1000)
     SUITE["constructor_$i"] = @benchmarkable BitSetTuple($i)
 end
 
-for i in (5, 10, 50, 100)
-    SUITE["intersect_$i"] = @benchmarkable intersect!(l, r) setup = begin
+for i in (5, 10, 50, 100, 1000)
+    SUITE["intersect!_$i"] = @benchmarkable intersect!(l, r) setup = begin
         l = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
         r = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
     end evals=1
 end
 
-for i in (5, 10, 50, 100)
-    SUITE["union_$i"] = @benchmarkable union!(l, r) setup = begin
+for i in (5, 10, 50, 100, 1000)
+    SUITE["union!_$i"] = @benchmarkable union!(l, r) setup = begin
         l = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
         r = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
     end evals=1
 end
 
-for i in (5, 10, 50, 100)
+for i in (5, 10, 50, 100, 1000)
+    SUITE["intersect_$i"] = @benchmarkable intersect(l, r) setup = begin
+        l = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
+        r = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
+    end evals=1
+end
+
+for i in (5, 10, 50, 100, 1000)
+    SUITE["union_$i"] = @benchmarkable union(l, r) setup = begin
+        l = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
+        r = BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i))
+    end evals=1
+end
+
+for i in (5, 10, 50, 100, 1000)
     SUITE["is_valid_partition_$i"] = @benchmarkable is_valid_partition(BitSetTuple(collect(Tuple(unique(rand(1:$i, rand(1:$i)))) for _ in 1:$i)))
 end
 
-for i in (2, 5, 10, 100)
+for i in (5, 10, 50, 100, 1000)
     SUITE["bounded_constructor_$i"] = @benchmarkable BoundedBitSetTuple($i)
 end
 
-for i in (5, 10, 50, 100)
-    SUITE["bounded_intersect_$i"] = @benchmarkable intersect!(l, r) setup = begin
+for i in (5, 10, 50, 100, 1000)
+    SUITE["bounded_intersect!_$i"] = @benchmarkable intersect!(l, r) setup = begin
         l = BoundedBitSetTuple(rand(Bool, ($i, $i)))
         r = BoundedBitSetTuple(rand(Bool, ($i, $i)))
     end evals=1
 end
 
-for i in (5, 10, 50, 100)
-    SUITE["bounded_union_$i"] = @benchmarkable union!(l, r) setup = begin
+for i in (5, 10, 50, 100, 1000)
+    SUITE["bounded_union!_$i"] = @benchmarkable union!(l, r) setup = begin
         l = BoundedBitSetTuple(rand(Bool, ($i, $i)))
         r = BoundedBitSetTuple(rand(Bool, ($i, $i)))
     end evals=1
 end
 
-for i in (5, 10, 50, 100)
+for i in (5, 10, 50, 100, 1000)
+    SUITE["bounded_intersect_$i"] = @benchmarkable intersect(l, r) setup = begin
+        l = BoundedBitSetTuple(rand(Bool, ($i, $i)))
+        r = BoundedBitSetTuple(rand(Bool, ($i, $i)))
+    end evals=1
+end
+
+for i in (5, 10, 50, 100, 1000)
+    SUITE["bounded_union_$i"] = @benchmarkable union(l, r) setup = begin
+        l = BoundedBitSetTuple(rand(Bool, ($i, $i)))
+        r = BoundedBitSetTuple(rand(Bool, ($i, $i)))
+    end evals=1
+end
+
+for i in (5, 10, 50, 100, 1000)
     SUITE["bounded_is_valid_partition_$i"] = @benchmarkable is_valid_partition(BoundedBitSetTuple(rand(Bool, ($i, $i))))
 end
--- scripts/bench.jl
+++ scripts/bench.jl
@@ -5,7 +5,12 @@ using PkgBenchmark
 result, name = if ARGS == []
     PkgBenchmark.benchmarkpkg(BitSetTuples), "current"
 else
-    BenchmarkTools.judge(BitSetTuples, ARGS[1]; judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05)), ARGS[1]
+    BenchmarkTools.judge(
+        BitSetTuples,
+        ARGS[1];
+        judgekwargs = Dict(:time_tolerance => 0.1, :memory_tolerance => 0.05),
+    ),
+    ARGS[1]
 end
 
 export_markdown("benchmark_vs_$(name)_result.md", result)
